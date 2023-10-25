c50e392a336a612da3afc36f10c58d278e0d9e08
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-25 19:21:56 +0300

    CI flows

--- src/rmcli/console.py
+++ src/rmcli/console.py
@@ -15,7 +15,7 @@ LOGGER = logging.getLogger(__name__)
 @click.option("-l", "--loglevel", help="Python log level, 10=DEBUG, 20=INFO, 30=WARNING, 40=CRITICAL", default=30)
 @click.option("-v", "--verbose", count=True, help="Shorthand for info/debug loglevel (-v/-vv)")
 def rmcli_cli(loglevel: int, verbose: int) -> None:
-    """CLI interface to RASENMAEHER API """
+    """CLI interface to RASENMAEHER API"""
     if verbose == 1:
         loglevel = 20
     if verbose >= 2:
